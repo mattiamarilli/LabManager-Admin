82362701f2d9e67959b8672d5fe7b0200c1d03ca
mattiamarilli <mattiamarilli@gmail.com>
2019-03-04 16:41:12 +0100

    added close currentsession functions but they dont work

--- src/app/services/teacher.service.ts
+++ src/app/services/teacher.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
+import {formatDate} from '@angular/common';
 
 
 @Injectable({
@@ -11,12 +12,18 @@ export class TeacherService {
   constructor(private http: HttpClient) { }
 
   url:string = 'http://marilliaws.ddns.net/qrmarconi-backend/';
-  
+ 
 
   getActiveSessions(){
     let headers = new HttpHeaders({
     });
     return this.http.get(this.url + "session.php", { headers: headers });
+  }
+
+  closeCurrentSession(ses_id:number,ses_chiusura:string){
+
+    console.log(this.url + "closecurrentsession.php?ses_id=" + ses_id + "&ses_chiusura= " + ses_chiusura);
+    return this.http.get(this.url + "closecurrentsession.php?ses_id=" + ses_id + "&ses_chiusura= " + ses_chiusura);
   }
 
 }
--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TeacherService } from '../services/teacher.service';
-
+import {formatDate} from '@angular/common';
 import { Session } from '../model';
 @Component({
   selector: 'app-session',
@@ -13,7 +13,13 @@ export class SessionComponent implements OnInit {
 
   sessions:Session;
 
+  stopSession()
+  { let date = formatDate(new Date(), 'yyyy-MM-dd h:MM:ss', 'en');
+    this.teacherservice.closeCurrentSession(this.sessions[0].ses_id,date);
+  }
+
   ngOnInit() {
+
     this.teacherservice.getActiveSessions().subscribe((data: Session ) => {
 			this.sessions = data;
 		}
