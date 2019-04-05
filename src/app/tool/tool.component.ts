a8647a6625e683cfaf71e1eeda2d048b77d53f37
Marco Trambusti <Marco Trambusti@meucci09.local>
2019-04-05 10:00:54 +0200

    work in progress but i push cause i nead to take this at home

--- src/app/tool/tool.component.ts
+++ src/app/tool/tool.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {ToolService} from '../services/tool.service'
-import { Utensili } from '../model'
+import { Utensili, Categorie } from '../model'
 import { Utensile } from '../model_body'
+  @Component({
+    selector: 'app-tool',
+    templateUrl: './tool.component.html',
+    styleUrls: ['./tool.component.css']
+  })
+  export class ToolComponent implements OnInit {
 
-@Component({
-  selector: 'app-tool',
-  templateUrl: './tool.component.html',
-  styleUrls: ['./tool.component.css']
-})
-export class ToolComponent implements OnInit {
+    constructor(private toolService:ToolService) { }
+    id_categoria:number;
+    Categorie:Categorie[];
+    Utensili:Utensili[];
+    utensile:Utensile;
+    
+    nomec: string;
+    nomeu:string;
 
-  constructor(private toolService:ToolService) { }
+    ngOnInit() {
+      this.Categorie = [
+       {id_categoria: 1, nome: 'dummyClass1'},
+       {id_categoria: 2, nome: 'dummyClass2'},
+       {id_categoria: 3, nome: 'dummyClass3'},
+       {id_categoria: 4, nome: 'dummyClass4'},
+     ];
+   }
+    setNewTool()
+    {
+      this.toolService.setUtensile(this.utensile);
+    }
 
-  ngOnInit() {
-  }
 
 }
