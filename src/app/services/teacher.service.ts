822b3721c618c3238caf623db06876f889e90e32
mattiamarilli <mattiamarilli@gmail.com>
2019-04-18 12:19:32 +0200

    working on teacher side

--- src/app/services/teacher.service.ts
+++ src/app/services/teacher.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-
+import { environment } from '../../environments/environment';
 import {formatDate} from '@angular/common';
-
+import {Docenti} from '../model'
+import {Docente} from '../model_body'
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,32 @@ export class TeacherService {
 
   constructor(private http: HttpClient) { }
 
+  getDocenti(): Observable<Docenti[]>{
+    let headers = new HttpHeaders({
+    });
+    return this.http.get<Docenti[]>(environment.apiUrl + '/admin/docente');
+  }
+
+  setDocente(docente:Docente){
+    let headers = new HttpHeaders({});
+    return this.http.post(`/admin/docente`, JSON.stringify(docente), { headers: headers });
+  }
+
+  modifyDocente(docente:Docente,id_docente:number){
+   
+      let headers  =new HttpHeaders({});
+
+      let body = {
+        'id_docente':id_docente,
+        'nome':docente.nome,
+        'admin': docente.admin
+      };
+      return this.http.put('/admin/docente', JSON.stringify(body), { headers: headers });
+    
+  }
+
+  deleteDocente(id_docente:number){
+    let headers  =new HttpHeaders({});
+    return this.http.request('delete', `/admin/docente`, { body: { headers: headers, id_docente: id_docente } });
+  }
 }
--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../services/student.service'
 import { ClassService } from '../services/class.service'
 import {Classi} from '../model';
-import {Studenti} from '../model';
-import {Studente} from '../model_body';
+import {Docenti} from '../model'
+import {Docente} from '../model_body'
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import {TeacherService} from '../services/teacher.service'
 @Component({
   selector: 'app-teacher',
   templateUrl: './teacher.component.html',
@@ -12,91 +13,39 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 })
 export class TeacherComponent implements OnInit {
 
-  constructor(private studentService:StudentService,private classService:ClassService, private modalService: NgbModal) { }
+  constructor(private teacherService:TeacherService,  private modalService: NgbModal) { }
 
-  studenti:Studenti[];
-  studente:Studente;
-  warning:string;
-  id_studente:number;
+  docenti:Docenti[];
+  docente:Docente;
+  id_docente:number;
 
   //modal
   nome:string = '';
-  cognome:string = '';
-  id_classe:number;
+  admin:boolean = true;
 
   ngOnInit() {
-      this.studente = new Studente();
-      this.studentService.getStudenti().subscribe((data: Studenti []) => {
-        this.studenti = data;
+      this.docente = new Docente();
+      this.teacherService.getDocenti().subscribe((data: Docenti []) => {
+        this.docenti = data;
       });
-      this.studentService.loadStudenti();
   }
 
-  /*
-  getStudentByClass()
-  {
-    this.studentService.getStudenti().subscribe((data:Studenti[])=>{
 
-      for(let dato of data)
-        {
-          if(dato.id_classe = this.id_classe)
-            this.studenti.push(dato);
-        }
-    })
-  }
-  */
-
-  addStudent(){
-    this.studente.nome = this.nome;
-    this.studente.cognome = this.cognome;
-    this.studente.id_classe = this.id_classe;
-    this.studentService.setStudent(this.studente).subscribe(data => {
-      if(data['code'] == 200){
-        this.studentService.loadStudenti();
-        this.modalService.dismissAll('Reason');
-      }else{
-        this.modalService.dismissAll('Reason');
-        this.warning = data['message'];
-      }
-    });
+  addDocente(){
+    this.docente.nome = this.nome;
+    this.docente.admin = this.admin;
+    this.teacherService.setDocente(this.docente).subscribe();
   }
 
   modifyStudent(){
-    this.studente.nome = this.nome;
-    this.studente.cognome = this.cognome;
-    this.studente.id_classe = this.id_classe;
-    this.studentService.modifyStudent(this.studente, this.id_studente).subscribe(data => {
-      if(data['code'] == 200){
-        this.studentService.loadStudenti();
-        this.modalService.dismissAll('Reason');
-      }else{
-        this.modalService.dismissAll('Reason');
-        this.warning = data['message'];
-      }
-    });
+    this.docente.nome = this.nome;
+    this.docente.admin = this.admin;
+    this.teacherService.modifyDocente(this.docente,this.id_docente).subscribe();
+   
   }
 
-  //Modal
-  open(content){
-    this.nome = '';
-    this.cognome = '';
-    this.id_classe = null;
-    this.id_studente = null;
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-titile'});
+  deleteDocente(){
+    this.teacherService.deleteDocente(this.id_docente).subscribe();
   }
 
-  openStudentModify(modify, id_studente){
-    this.nome = '';
-    this.cognome = '';
-    this.id_classe = null;
-    this.id_studente = id_studente;
-    for(let s of this.studenti){
-      if(id_studente == s.id){
-        this.nome = s.nome;
-        this.cognome = s.cognome;
-        this.id_classe = s.id_classe;
-      }
-    }
-    this.modalService.open(modify, {ariaLabelledBy: 'modal-basic-titile'});
-  }
 }
