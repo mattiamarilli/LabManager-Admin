f7a0c9f2049d0bc2a06d78307c9750853868d3a2
mattiamarilli <mattiamarilli@gmail.com>
2019-05-10 09:04:00 +0200

    mod user comp

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthUser} from '../model'
 import {AuthenticationService} from '../services/authentication.service'
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -8,13 +9,20 @@ import {AuthenticationService} from '../services/authentication.service'
 })
 export class UserComponent implements OnInit {
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService,private toastr: ToastrService) { }
 
   user:AuthUser = new AuthUser;
   oldpassword:string;
   newpassword:string;
   modifyPassword(){
-    this.authService.modifyPassword(this.user.id,this.oldpassword,this.newpassword).subscribe();
+    this.authService.modifyPassword(this.user.id,this.oldpassword,this.newpassword).subscribe((data:any)=>{ 
+      if(data.code == 200)
+      this.toastr.success('Password Modificata', 'Successo');
+      else if(data.code == 501)
+      this.toastr.error('Vecchia password non corretta', 'Errore');
+      else if(data.code == 500)
+      this.toastr.error('Password non modificata', 'Errore');
+    });
   }
 
   ngOnInit() {
